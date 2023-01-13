fd2b2c6943a4d3cd6ffd16d5c218f3b5ae2edfc5
Sr.Sagan <71529504+SrSagan@users.noreply.github.com>
2023-01-13 19:44:09 -0300

    Remade the whole code
    now using FFMPEG-python library

--- cdriper.py
+++ cdriper.py
@@ -1,132 +1,91 @@
-import sys, os
-import subprocess
-import pycdio, cdio
+import ffmpeg
+import sys
 
+cdName="/dev/cdrom"
+format = 'wav'
+tracks = []
 
-cdName = '/dev/cdrom' #variable para localizacion de cdrom default: /dev/cdrom
-format = 'wav' #variable para formato de salida default: wav
-isTrackNum = False  #variable para checkear si se aclaro un numero de track
-faso_array = [] #array para guardar los diferentes tracks
+help= """Arguments:
+-cd         Specify cdrom to use, use: -cd <path>
+-"number"   Specify track number, use: -1:2...
+-format     Selects the format of output file, default is wav
+-h,?        Displays help"""
 
+def numberGrab(arg):#separates track numbers
+    looping=True
+    while looping:
+        length = len(arg)  
+        x = arg.rfind(":") #searches for the : dividing the numbers
+        if(x != -1):
+            trackNum = arg[x+1:length]
+            arg=arg[:x] #grabs the last number and removes it from the string
+        else:
+            trackNum = arg[1:length]
+            looping=False
+        if(trackNum.isnumeric()):
+            if(int(trackNum) not in tracks):
+                tracks.append(int(trackNum))
+        else:
+            print("'%s' is not a track number" %trackNum)
+            sys.exit(1)
 
-help = """ Parameters: 
--cd 		changes what cdrom cdriper uses. eg: -cd /dev/cdrom 
--"num" 		selects a single track from the cd. eg: -2 (only converts second track)
--open 		opens cdrom
--format 	selects the output file format default: wav. eg: -format flac
--h 			displays help.
--? 			displays help.""" #string con toda la info de ayuda
+args = sys.argv
 
-d = cdio.Device(cdName) #Variable usada luego para abrir el cd
+if(len(args) > 1): #checks arguments
+    args.pop(0)
+    for arg in args:
+        if(arg.find("-") == -1):
+            print("Invalid argument '%s'" %arg)
+            sys.exit(1)
+        
+        if(arg == '-cd'):
+            index = args.index(arg)
+            cdName=args[index+1]
+            args.pop(index+1)#after grabbing the argument it pops it to save time and code
+        
+        elif(arg[1].isnumeric()):
+            numberGrab(arg)
+        
+        elif(arg == '-format'):
+            index = args.index(arg)
+            format=args[index+1]
+            args.pop(index+1)
+        
+        elif(arg in ["?", "h", 'help']):
+            print(help)
+            sys.exit(1)
+        
+        else:
+            print("Invalid argument '%s'\n" %arg)
+            print(help)
+            sys.exit(1)
 
-faso = str(sys.argv) #se chequea si hay argumentos
-argument = faso.find('-') #se busca - para señalizar un comando
+print("Reading CD") #informs the user
 
+try:
+    cd_info_raw = ffmpeg.probe(cdName, f='libcdio', print_format='json', show_chapters=None) #reads json from the CD
+except ffmpeg.Error as e:
+    print(str(e.stderr), file=sys.stderr)
+    sys.exit(1)
 
-if argument != -1: #si hay un argumentos
+toConvert=[]
+if(len(tracks) > 0): #checks the tracks to convert and if they even exist
+    for track in tracks:
+        if(track > len(cd_info_raw["chapters"])): 
+            print("'%d' is not a valid track number" %track)
+            sys.exit(1)
+        toConvert.append(cd_info_raw["chapters"][track-1])
 
-	faso = faso[argument:]
-	firstLine = faso.find("-")
-	secondLine = faso.rfind("-")
-	
-	while True: #se comienza un loop para checkear argumentos 
+else:
+    toConvert = cd_info_raw["chapters"]
 
-		firstLine = faso.find("-")
-		secondLine = faso.rfind("-") # se checkea si hay mas de 1 argumento
-
-		x = faso.find("-")
-		faso = faso[x+1:]
-		x = faso.find("'")
-		faso2 = faso[:x] #separa el argumento y lo pasa a faso2
-			
-
-#checkeo de argumentos
-#-----------------------------------------------------------------------------------------------------------------------------------------
-		
-
-		if faso2 == "open": #checkea si dice open
-			d.eject_media_drive()
-			exit()
-
-		elif faso2 == 'cd': #checkea si dice cd
-			x = faso.find(",")
-			cdName = faso[x+3:]
-			x = cdName.find("'")
-			cdName = cdName[:x] #se aisla en nombre del cd-rom
-
-		elif faso2 == 'format': #se checkea si dice format
-			x = faso.find(",")
-			format = faso[x+3:]
-			x = format.find("'")
-			format = format[:x] #se aisla el formato
-
-		elif faso2 == 'h' or faso2 == '?':
-			print(help) #si la palabra es h o ? se imprime la ayuda
-			exit()
-
-		while True: #se comienza otro loop
-
-			fasofind = faso2.find(":")
-			x = faso2.rfind(":")
-			j = len(faso2)
-			faso_int = (faso2[x+1:j]) #se busca y se aisla cada valor de track ingresado
-
-			isTrackNum = faso_int.isnumeric() #se checkea que lo ingresado sea un valor de track
-			if isTrackNum == True:
-
-				faso_array.append(int(faso_int)) #se pone el valor de track en una array
-				faso2 = faso2[:x]
-
-			if fasofind == -1: #en caso de no encontrarse : se rompe el loop, esto se hace asi para que el usuario tambien pueda ingresar un solo numero de track
-				break 
-			
-
-		else: #si no dice ninguna de las anteriores dice invalid argument
-			print("Invalid Argument:", faso2)
-			exit()		
-		
-		if firstLine == secondLine: #si solo queda un argumento se rompe el loop
-			break
-
-#------------------------------------------------------------------------------------------------------------------------------------------
-
-#Conversion
-#------------------------------------------------------------------------------------------------------------------------------------------
-command = "ffprobe -f libcdio -i " + cdName + " -print_format json -show_chapters -loglevel error" #se corre ffprobe para conseguir la informacion del cd
-
-subprocess = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE) #se lee la salida del comento	
-cd_info = subprocess.stdout.read()
-cd_info_str = str(cd_info) #se guarda la salida del comando en un string
-
-i=cd_info_str
-j=cd_info_str.find("start_time") #se busca en la info del cd el comienzo del primer track
-while(j > 0): #loopea mientras encuentre tracks
-
-	j=i.find("start_time") 
-	if j != -1:
-		j=i.find("start_time")
-		i = i[j+14:]
-		x = i.find('"') 
-		start_time = i[:x] #se aisla el tiempo de comienzo
-		j=i.find("end_time") #se busca el tiempo final
-		i = i[j+12:]
-		x = i.find('"')
-		end_time = i[:x] #se aisla el tiempo final
-		j=i.find("title") #se busca el titulo del track (track X)
-		i = i[j+9:]
-		x = i.find('"')
-		name = i[:x] #se aisla en nombre
-		if argument != -1: #si un argumento fue pesto
-			if isTrackNum == True: #se checkea si el argumento son tracks
-				j=name.find(" ")
-				trackNum = name[j+1:]
-				x = trackNum.find('') #se encuentra el numero de track dentro de la info del cd
-				if trackNum.isnumeric() == True:
-					trackNum = int(trackNum) #se checke y aisla el numero del track del nombre 
-					if trackNum in faso_array: #si el numero del track esta en la array de tracks del usuario se convierte
-						convert = 'ffmpeg -f libcdio -ss '+start_time+' -to '+end_time+' -i '+cdName+' "'+name+'.'+format+'"'
-						os.system(convert)
-			else: #en caso de que no haya argumento se convierten todos los tracks
-				convert = 'ffmpeg -f libcdio -ss '+start_time+' -to '+end_time+' -i '+cdName+' "'+name+'.'+format+'"'
-				os.system(convert)
-#------------------------------------------------------------------------------------------------------------------------------------------
+print("Starting to rip")
+for track in toConvert:
+    trackIn = ffmpeg.input(cdName, f='libcdio', ss=track["start_time"], to=track["end_time"])
+    if(format == 'mp3'):
+        trackOut = ffmpeg.output(trackIn, track["tags"]["title"]+"."+format ,audio_bitrate="325k") #makes 320kbps mp3
+    else:
+        trackOut = ffmpeg.output(trackIn, track["tags"]["title"]+"."+format)
+    print("Ripping %s..." %track["tags"]["title"])
+    ffmpeg.run(trackOut, quiet=True, overwrite_output=True) #rips
+    print("Ripped successfully")
